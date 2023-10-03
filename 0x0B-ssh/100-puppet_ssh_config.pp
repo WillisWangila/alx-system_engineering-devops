3fd7de6e9cdc6afcb4e0da940fb4c0f41defa6e1
WillisWangila <w.wangila@gmail.com>
2023-10-04 00:22:01 +0300

    Initial commit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Set up my client config file
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+}
